3ca58473e7414e27dab8fadba0bced9ffad9f363
Mad-Sweeny <66201815+Mad-Sweeny@users.noreply.github.com>
2023-05-10 12:45:06 +0530

    Update prediction_api.py

--- code/prediction_api.py
+++ code/prediction_api.py
@@ -3,14 +3,14 @@ import pickle
 import numpy as np
 import json
 
-app = Flask(__name__)
-with open(r'..\trained_model\random_forest_regressor_model.pkl','rb') as f:
+#app = Flask(__name__)
+with open(r'trained_model/random_forest_regressor_model.pkl','rb') as f:
     model = pickle.load(f)
 # model = pickle.load(r'..\trained_model\random_forest_regressor_model.pkl')
 
 
-@app.route('/predict')
-def predict():
+#@app.route('/predict')
+def predict(requests):
     vol_moving_avg = request.args.get('vol_moving_avg')
     adj_close_rolling_med = request.args.get('adj_close_rolling_med')
     features = [vol_moving_avg,adj_close_rolling_med]
@@ -19,5 +19,5 @@ def predict():
     
     return jsonify({'predictions' : predictions})   
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
